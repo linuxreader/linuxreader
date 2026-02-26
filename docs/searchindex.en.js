2608c4a1d13b4133fc776ba8086d9b2e4d4557eb
linuxreader <tdavetech@gmail.com>
2026-02-26 05:06:43 -0700

    First Update

---
+++ docs/searchindex.en.js
@@ -0,0 +1,18 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Linux Documentation Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/"
+  },
+  {
+    "breadcrumb": "Linux Documentation Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/"
+  }
+]
